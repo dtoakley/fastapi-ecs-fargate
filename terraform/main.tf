a2c80900fe2f51506119de4f7974eeb6a16c8b95
Dan Lewis-Toakley <d.lewistoakley@gmail.com>
2022-06-03 15:48:10 -0400

    updated tf and circle

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,7 +5,7 @@ locals {
 }
 
 resource "aws_ecr_repository" "fastapi-ecs-fargate-registry" {
-  name                 = "${var.aws_resource_prefix}-registry"
+  name                 = "${var.AWS_RESOURCE_NAME_PREFIX}-registry"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
@@ -13,25 +13,42 @@ resource "aws_ecr_repository" "fastapi-ecs-fargate-registry" {
   }
 }
 
+resource "aws_acm_certificate" "fastapi-ecs-fargate-ssl-cert" {
+  domain_name       = "local"
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
 module "base-network" {
   source                                      = "cn-terraform/networking/aws"
   version                                     = "2.0.7"
-  name_preffix                                = "${var.aws_resource_prefix}-networking"
-  vpc_cidr_block                              = "192.168.0.0/16"
-  availability_zones                          = ["us-east-1a", "us-east-1b"]
-  public_subnets_cidrs_per_availability_zone  = ["192.168.0.0/19", "192.168.32.0/19"]
-  private_subnets_cidrs_per_availability_zone = ["192.168.128.0/19", "192.168.160.0/19"]
+  name_preffix                                = "${var.AWS_RESOURCE_NAME_PREFIX}-networking"
+  vpc_cidr_block                              = var.cidr
+  availability_zones                          = var.availability_zones
+  public_subnets_cidrs_per_availability_zone  = var.public_subnets
+  private_subnets_cidrs_per_availability_zone = var.private_subnets
 }
 
 module "ecs-fargate" {
   source                                         = "cn-terraform/ecs-fargate/aws"
   version                                        = "2.0.41"
-  name_prefix                                    = var.aws_resource_prefix
+  name_prefix                                    = var.AWS_RESOURCE_NAME_PREFIX
   vpc_id                                         = module.base-network.vpc_id
-  container_image                                = "${local.account_id}.dkr.ecr.${var.default_region}.amazon.com/${aws_ecr_repository.fastapi-ecs-fargate-registry.name}"
-  container_name                                 = "${var.aws_resource_prefix}-container"
+  container_image                                = "${local.account_id}.dkr.ecr.${var.default_region}.amazonaws.com/${aws_ecr_repository.fastapi-ecs-fargate-registry.name}"
+  container_name                                 = "${var.AWS_RESOURCE_NAME_PREFIX}-container"
   public_subnets_ids                             = module.base-network.public_subnets_ids
   private_subnets_ids                            = module.base-network.private_subnets_ids
   s3_bucket_server_side_encryption_sse_algorithm = "AES256"
+  container_cpu                                  = var.container_cpu
+  container_memory                               = var.container_memory
+  container_memory_reservation                   = var.container_memory_reservation
+  desired_count                                  = var.service_desired_count
+  port_mappings                                  = var.port_mappings
+  lb_http_ports                                  = var.lb_http_ports
+  default_certificate_arn                        = aws_acm_certificate.fastapi-ecs-fargate-ssl-cert.arn
+  ssl_policy                                     = "ELBSecurityPolicy-2016-08"
 }
 
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,7 +3,102 @@ variable "default_region" {
   default = "us-east-1"
 }
 
-variable "aws_resource_prefix" {
+variable "AWS_RESOURCE_NAME_PREFIX" {
   type    = string
   default = "fastapi-ecs-fargate"
 }
+
+variable "availability_zones" {
+  description = "a comma-separated list of availability zones, defaults to all AZ of the region, if set to something other than the defaults, both private_subnets and public_subnets have to be defined as well"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b"]
+}
+
+variable "cidr" {
+  description = "The CIDR block for the VPC."
+  type        = string
+  default     = "192.168.0.0/16"
+}
+
+variable "private_subnets" {
+  description = "a list of CIDRs for private subnets in your VPC, must be set if the cidr variable is defined, needs to have as many elements as there are availability zones"
+  type        = list(string)
+  default     = ["192.168.128.0/19", "192.168.160.0/19"]
+}
+
+variable "public_subnets" {
+  description = "a list of CIDRs for public subnets in your VPC, must be set if the cidr variable is defined, needs to have as many elements as there are availability zones"
+  type        = list(string)
+  default     = ["192.168.0.0/19", "192.168.32.0/19"]
+}
+
+variable "service_desired_count" {
+  description = "Number of tasks running in parallel"
+  type        = number
+  default     = 2
+}
+
+variable "container_cpu" {
+  description = "The number of cpu units used by the task"
+  type        = number
+  default     = 256 # If unset, default is 1024 (1 vCPU)
+}
+
+variable "container_memory" {
+  description = "The amount of memory (in MiB) the container is allows to use"
+  type        = number
+  default     = 512 # If unset, default is 4096 (4 GB)
+}
+
+variable "container_memory_reservation" {
+  description = "The amount of memory (in MiB) reserved for the container"
+  type        = number
+  default     = 256 # If unset, default is 2048 (2 GB)
+}
+
+variable "port_mappings" {
+  description = "The port mappings to configure for the container"
+  type = list(object({
+    containerPort = number
+    hostPort      = number
+    protocol      = string
+  }))
+  default = [
+    {
+      containerPort = 80
+      hostPort      = 80
+      protocol      = "tcp"
+    },
+    {
+      containerPort = 443
+      hostPort      = 443
+      protocol      = "tcp"
+    }
+  ]
+}
+
+variable "lb_http_ports" {
+  description = "Map containing objects to define listeners behaviour based on type field. If type field is `forward`, include listener_port and the target_group_port. For `redirect` type, include listener port, host, path, port, protocol, query and status_code. For `fixed-response`, include listener_port, content_type, message_body and status_code"
+  type        = map(any)
+
+  default = {
+    default_http = {
+      type          = "redirect"
+      listener_port = 80
+      port          = 443
+      protocol      = "HTTPS"
+      status_code   = "HTTP_301"
+    }
+  }
+}
+
+#variable "lb_https_ports" {
+#  description = "Map containing objects with two fields, listener_port and the target_group_port to redirect HTTPS requests"
+#  type        = map(any)
+#  default = {
+#    default_http = {
+#      listener_port     = 443
+#      target_group_port = 80
+#    }
+#  }
+#}
